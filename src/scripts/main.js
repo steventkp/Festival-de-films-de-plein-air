4d70dd85b7b1b9ab1225285ba6d2d7643fb8c621
Steven <steven.tchakpe@gmail.com>
2018-10-22 19:17:28 +0200

    Added changes on hover preinscription button + fixed location pointer on google map

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -130,7 +130,7 @@ function initMap() {
             }
         ]
     });
-    var image = '../../img/location-pointer.png';
+    var image = '../../festival/img/location-pointer.png';
     var marker = new google.maps.Marker({
         position: latLng,
         map: map,
